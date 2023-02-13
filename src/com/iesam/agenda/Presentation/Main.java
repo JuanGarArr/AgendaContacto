797ae39a7585dc988997fe9e4f7ef85272953fb4
Juan' 'García Arruche <7.515@amav.net>
2023-02-13 18:44:30 +0200

    Comienzo Proyecto

---
+++ src/com/iesam/agenda/Presentation/Main.java
@@ -0,0 +1,7 @@
+package com.iesam.agenda.Presentation;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/iesam/agenda/Presentation/TerminalView.java
@@ -0,0 +1,4 @@
+package com.iesam.agenda.Presentation;
+
+public class TerminalView {
+}
